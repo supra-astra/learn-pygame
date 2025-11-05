a441d2d903637e6c597559be737d98bc375e4d85
Sagnik Chatterjee <sagnikchatterjee607@gmail.com>
2025-11-05 16:51:35 -0500

    refactor: moved constants to other file

--- main.py
+++ main.py
@@ -3,42 +3,9 @@ os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "Github Gameoff 2025"
 
 import pygame
 from sys import exit
+from constants import *
 
 
-# width and height of our game window
-width = 800 
-height = 400
-
-#set the title of window
-pygame.display.set_caption('Basic Game')
-
-#clock helps us with time and framerate
-clock = pygame.time.Clock()
-
-
-# surface made with color
-surface_w = 100
-surface_h = 200
-test_surface = pygame.Surface((surface_w,surface_h))
-# add color to it
-test_surface.fill('Red')
-
-sky_surface = pygame.image.load("assets/sky.png")
-ground_surface = pygame.image.load("assets/ground.png")
-
-snail_surface = pygame.image.load("assets/snail/snail1.png")
-player_surface = pygame.image.load("assets/player/player_walk_1.png")
-
-# left,top,width,height 
-#player_rect = pygame.Rect()
-# get rectangle the same size as it
-player_rect = player_surface.get_rect(topleft = (80,200))
-
-snail_x_pos = 600
-snail_y_pos = 250
-
-player_x_pos = 800
-player_y_pos = 250
 
 def main():
     # instantiesaties pygame , play sounds and images
